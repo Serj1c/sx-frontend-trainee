477a2be19a09dfde84f6ad723512cf6968faac48
Serj1c <sergej.a.isaev@gmail.com>
2021-09-11 08:58:12 +0300

    little updates

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## sx-frontend-trainee
 Test exercise on a position of an intern in Avito: Hacker News API  
-Not everything is implemented --> some of the points regarding comments aren't done
+
+To run the app:
+make dev
--- src/components/StoryPage/StoryPage.tsx
+++ src/components/StoryPage/StoryPage.tsx
@@ -1,16 +1,36 @@
 import { Link } from 'react-router-dom'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button } from 'components/common'
 import styles from './StoryPage.module.css'
 import { ListOfComments } from './components'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchStoryRedux } from 'redux/story/storyActions'
 
 export const StoryPage:React.FunctionComponent = (): JSX.Element => {
 
     const story = useSelector((state: any /* TODO */) => state.story.story)
+    const dispatch = useDispatch()
 
     let date = new Date(story?.time * 1000)
     let dateUpdated = date.toUTCString()
+
+    useEffect(() => {
+        try {
+            const interval = setInterval(() => {
+                dispatch(fetchStoryRedux(story.id))
+            }, 60000);
+            return () => {
+                clearInterval(interval)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }, [dispatch, story.id])
+
+    /* TODO handle update of comments the other way */
+    const handleClick = () => {
+        dispatch(fetchStoryRedux(story.id))
+    }
 
     return (
         <div className={styles.container}>
@@ -30,7 +50,7 @@ export const StoryPage:React.FunctionComponent = (): JSX.Element => {
                     <li><i>published:</i> <strong>{dateUpdated}</strong></li>
                     <li><i>{story.descendants} comment{story.descendants === 1 ? " " : "s"}</i></li>
                 </ul>
-                <Button>Refresh</Button>
+                <Button onClick={handleClick}>Get comments</Button>
                 {story.kids ? story.kids.map((commentId: number) => (
                     <ListOfComments key={commentId} commentId={commentId}/>)) : <i>Nobody commented yet</i> }
             </div>
